c465cc82ebb6742c10a83f6395ed7020450f6304
Bui Nhat <buidinhnhat2071@gmail.com>
2020-12-06 14:54:18 +0700

    #change auth setting

--- src/app/fuse-layouts/shared-components/Logo.js
+++ src/app/fuse-layouts/shared-components/Logo.js
@@ -3,27 +3,27 @@ import { Typography } from "@material-ui/core";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/styles";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     "& .logo-icon": {
       width: 24,
       height: 24,
       transition: theme.transitions.create(["width", "height"], {
         duration: theme.transitions.duration.shortest,
-        easing: theme.transitions.easing.easeInOut
-      })
+        easing: theme.transitions.easing.easeInOut,
+      }),
     },
     "& .react-badge, & .logo-text": {
       transition: theme.transitions.create("opacity", {
         duration: theme.transitions.duration.shortest,
-        easing: theme.transitions.easing.easeInOut
-      })
-    }
+        easing: theme.transitions.easing.easeInOut,
+      }),
+    },
   },
   reactBadge: {
     backgroundColor: "rgba(0,0,0,0.6)",
-    color: "#61DAFB"
-  }
+    color: "#61DAFB",
+  },
 }));
 
 function Logo() {
@@ -33,7 +33,7 @@ function Logo() {
     <div className={clsx(classes.root, "flex items-center")}>
       <img
         className="logo-icon"
-        src="assets/images/logos/fuse.svg"
+        src="assets/images/logos/logo.svg"
         alt="logo"
       />
       <Typography
@@ -42,20 +42,6 @@ function Logo() {
       >
         ADMIN
       </Typography>
-      <div
-        className={clsx(
-          classes.reactBadge,
-          "react-badge flex items-center ml-12 mr-8 py-4 px-8 rounded"
-        )}
-      >
-        <img
-          className="react-logo"
-          src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
-          alt="react"
-          width="16"
-        />
-        <span className="react-text text-12 ml-4">React</span>
-      </div>
     </div>
   );
 }
