b8702a0d3c97b21088b3b35a845e4a75e66fe820
gywreb <longnh.gywreb@gmail.com>
2021-02-18 17:05:26 +0700

    implement get all user api

--- src/app/main/apps/user-management/store/actions/users.actions.js
+++ src/app/main/apps/user-management/store/actions/users.actions.js
@@ -3,16 +3,18 @@ import axios from "axios";
 export const GET_USERS = "[USER MANAGEMENT] GET USERS";
 export const SET_USER_SEARCH_TEXT = "[USER MANAGEMENT] SET USER SEARCH TEXT";
 
+const baseURL = process.env.REACT_APP_API_ENDPOINT;
+
 export const getUsers = () => {
-  const request = axios.get("/api/user/all");
+  const request = axios({ baseURL, url: "/user", method: "GET" });
 
   return (dispatch) =>
-    request.then((response) =>
+    request.then((response) => {
       dispatch({
         type: GET_USERS,
         payload: response.data,
-      })
-    );
+      });
+    });
 };
 
 export const setUserSearchText = (text) => {
